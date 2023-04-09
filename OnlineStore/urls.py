e5dba9d785ad964b836a65964b81a2910d746f2e
DJUgo13 <YOUR EMAIL ON GITHUB>
2023-04-09 13:00:24 +0600

    a

--- OnlineStore/urls.py
+++ OnlineStore/urls.py
@@ -13,21 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from products.views import main_view, products_view, product_create_view, products_detail_view
-from users.views import register_view, login_view, logout_view
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', main_view),
-                  path('products/', products_view),
-                  path('products/create/', product_create_view),
-                  path('products/<int:id>/', products_detail_view),
-                  path('users/register/', register_view),
-                  path('users/login/', login_view),
-                  path('users/logout/', logout_view)
+                  path('', include('products.urls2')),
+                  path('users/', include('users.urls3'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
